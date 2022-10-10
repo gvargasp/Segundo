8dff1435d7d7b87008d8168d01e9917aac53e25f
Gina Vargas <gvargasp@hotmail.com>
2022-10-10 01:13:51 -0300

    incorporando componente

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Testimonio() {
+  return (
+    <div className='contenedor-testimonio'>
+      <img
+        className='imagen-testimonio'
+        src={require('../imagenes/testimonio-robot1.jpg')}
+        alt='Foto de Robot1' />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>Robot1 en Iquique</p>
+        <p className='cargo-testimonio'>Analista</p>
+        <p className='texto-testimonio'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium, explicabo facere eligendi odit nobis accusamus rerum, molestias culpa ut delectus adipisci ex consequuntur quidem. Reiciendis autem quae maxime enim cumque.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
